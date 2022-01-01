e5e4311887436a8e2ced3d041119d2b97b2c2e57
CodeDoctorDE <codedoctor@linwood.dev>
2022-01-01 15:31:19 +0100

    fix export

--- packages/localizer/example/localizer_example.dart
+++ packages/localizer/example/localizer_example.dart
@@ -2,13 +2,15 @@ import 'package:localizer/localizer.dart';
 
 void main() {
   var localizer = Localizer();
-  localizer.loadMap("en", {"hello-world": "Hello world!"});
+  localizer.loadMap("en", {
+    "hello": ["Hello world!"]
+  });
   localizer.loadMap("es", {"hello-world": "Hola mundo!"});
   localizer.loadMap("fr", {"hello-world": "Bonjour le monde!"});
   localizer.loadMap("de", {"hello-world": "Hallo Welt!"});
 
-  print("EN: ${localizer.get("en", "hello-world")}");
-  print("ES: ${localizer.get("es", "hello-world")}");
+  print("EN: ${localizer.get("en", "hello.0")}");
+  print("ES: ${localizer.get("es", "hello.0")}");
   print("FR: ${localizer.get("fr", "hello-world")}");
   print("DE: ${localizer.get("de", "hello-world")}");
 }
--- packages/localizer/lib/localizer.dart
+++ packages/localizer/lib/localizer.dart
@@ -4,5 +4,4 @@
 library localizer;
 
 export 'src/localizer_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/localizer_locale.dart';

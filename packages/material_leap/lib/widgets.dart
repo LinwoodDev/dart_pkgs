99a7a290bd5bfb80e68a08de3ef9dec939bec63b
CodeDoctorDE <codedoctor@linwood.dev>
2023-05-06 19:06:18 +0200

    Add missing export

--- packages/material_leap/lib/widgets.dart
+++ packages/material_leap/lib/widgets.dart
@@ -2,6 +2,7 @@ library material_leap;
 
 export 'src/widgets/advanced_switch_list_tile.dart';
 export 'src/widgets/box_tile.dart';
+export 'src/widgets/exact_slider.dart';
 export 'src/widgets/date_time_field.dart';
 export 'src/widgets/header.dart';
 export 'src/widgets/leap_bottom_sheet.dart';

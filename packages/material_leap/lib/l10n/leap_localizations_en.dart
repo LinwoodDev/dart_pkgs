0a2893119b10f705d4971c98d4e9c12eb8856b02
CodeDoctorDE <codedoctor@linwood.dev>
2023-05-06 19:26:03 +0200

    Fix translation

---
+++ packages/material_leap/lib/l10n/leap_localizations_en.dart
@@ -0,0 +1,24 @@
+import 'leap_localizations.dart';
+
+/// The translations for English (`en`).
+class LeapLocalizationsEn extends LeapLocalizations {
+  LeapLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get color => 'Color';
+
+  @override
+  String get pin => 'Pin';
+
+  @override
+  String get delete => 'Delete';
+
+  @override
+  String get red => 'Red';
+
+  @override
+  String get green => 'Green';
+
+  @override
+  String get blue => 'Blue';
+}

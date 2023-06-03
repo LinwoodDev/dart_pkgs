5ebdc1ca9d5b60fb36a865ed2529bb57f18daff4
CodeDoctorDE <codedoctor@linwood.dev>
2023-06-03 17:14:07 +0200

    Add new helpers

--- packages/material_leap/lib/helpers.dart
+++ packages/material_leap/lib/helpers.dart
@@ -3,4 +3,5 @@ library material_leap;
 export 'src/helpers/color.dart';
 export 'src/helpers/date_time.dart';
 export 'src/helpers/icon.dart';
+export 'src/helpers/int.dart';
 export 'src/helpers/string.dart';
--- packages/material_leap/lib/src/helpers/date_time.dart
+++ packages/material_leap/lib/src/helpers/date_time.dart
@@ -27,4 +27,17 @@ extension DateTimeHelper on DateTime {
   int getDaysInMonth() {
     return DateTime(year, month + 1, 0).day;
   }
+
+  int get secondsSinceEpoch {
+    var ms = millisecondsSinceEpoch;
+    return (ms / 1000).round();
+  }
+
+  DateTime onlyDate() {
+    return DateTime(year, month, day);
+  }
+
+  static DateTime fromSecondsSinceEpoch(int seconds) {
+    return DateTime.fromMillisecondsSinceEpoch(seconds * 1000);
+  }
 }

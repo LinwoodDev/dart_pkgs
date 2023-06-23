7c79d5ab281f4afe92a8dadced6d7c5e501c0794
CodeDoctorDE <codedoctor@linwood.dev>
2023-06-23 15:31:44 +0200

    Add connect and disconnect stream

--- packages/networker/networker/lib/src/server.dart
+++ packages/networker/networker/lib/src/server.dart
@@ -2,6 +2,13 @@ part of 'connection.dart';
 
 abstract class NetworkerServer<T extends NetworkerConnection> {
   final Map<ConnectionId, (T, StreamSubscription<RawData>)> _connections = {};
+  final StreamController<ConnectionId> _connectController =
+      StreamController.broadcast();
+  final StreamController<ConnectionId> _disconnectController =
+      StreamController.broadcast();
+
+  Stream<ConnectionId> get connect => _connectController.stream;
+  Stream<ConnectionId> get disconnect => _disconnectController.stream;
 
   List<ConnectionId> get connectionIds => _connections.keys.toList();
 
@@ -16,6 +23,7 @@ abstract class NetworkerServer<T extends NetworkerConnection> {
         sendMessage(id, data);
       })
     );
+    _connectController.add(id);
     return true;
   }
 
@@ -23,6 +31,7 @@ abstract class NetworkerServer<T extends NetworkerConnection> {
   bool removeConnection(ConnectionId id, T connection) {
     if (_connections.containsKey(id)) return false;
     _connections.remove(id)?.$2.cancel();
+    _disconnectController.add(id);
     return true;
   }
 
@@ -33,4 +42,7 @@ abstract class NetworkerServer<T extends NetworkerConnection> {
   void sendMessage(ConnectionId id, RawData data) {
     getConnection(id)?.sendMessage(data);
   }
+
+  bool get isClosed;
+  void close();
 }

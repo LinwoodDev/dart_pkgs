723027ec49a65f68708e23336348695cf2bf79bb
CodeDoctorDE <codedoctor@linwood.dev>
2023-12-16 11:07:12 +0100

    Add box tile leading and trailing

--- packages/material_leap/lib/src/widgets/box_tile.dart
+++ packages/material_leap/lib/src/widgets/box_tile.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class BoxTile extends StatelessWidget {
   final Widget title, icon;
-  final Widget? subtitle;
+  final Widget? subtitle, leading, trailing;
   final double size, selectionWidth, selectionRadius;
   final bool selected;
   final GestureTapCallback? onTap;
@@ -11,9 +11,11 @@ class BoxTile extends StatelessWidget {
     super.key,
     required this.title,
     this.subtitle,
+    this.leading,
+    this.trailing,
     required this.icon,
     this.onTap,
-    this.size = 100,
+    this.size = 128,
     this.selected = false,
     this.selectionWidth = 4,
     this.selectionRadius = 12,
@@ -21,40 +23,64 @@ class BoxTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAliasWithSaveLayer,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(selectionRadius),
-      ),
-      child: InkWell(
-        onTap: onTap,
-        borderRadius: BorderRadius.circular(selectionRadius),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(
-                width: selectionWidth,
-                color: selected
-                    ? Theme.of(context).colorScheme.primary
-                    : Colors.transparent),
-            borderRadius: BorderRadius.circular(selectionRadius),
-          ),
-          child: SizedBox.square(
-            dimension: size,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+    return SizedBox.square(
+      dimension: size,
+      child: Align(
+        child: SizedBox.square(
+          dimension: size,
+          child: Card(
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(selectionRadius),
+            ),
+            child: Stack(
               children: [
-                Expanded(
-                  child: IconTheme(
-                    data: Theme.of(context).iconTheme.copyWith(size: 32),
-                    child: icon,
+                SizedBox.square(
+                  dimension: size,
+                  child: InkWell(
+                    onTap: onTap,
+                    borderRadius: BorderRadius.circular(selectionRadius),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                            width: selectionWidth,
+                            color: selected
+                                ? Theme.of(context).colorScheme.primary
+                                : Colors.transparent),
+                        borderRadius: BorderRadius.circular(selectionRadius),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          IconTheme(
+                            data:
+                                Theme.of(context).iconTheme.copyWith(size: 32),
+                            child: icon,
+                          ),
+                          Column(
+                            children: [
+                              DefaultTextStyle(
+                                style: Theme.of(context).textTheme.bodySmall!,
+                                child: subtitle ?? const SizedBox(),
+                              ),
+                              title,
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                const SizedBox(height: 16),
-                DefaultTextStyle(
-                  style: Theme.of(context).textTheme.bodySmall!,
-                  child: subtitle ?? const SizedBox(),
-                ),
-                title,
+                if (leading != null)
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: leading!,
+                  ),
+                if (trailing != null)
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: trailing!,
+                  ),
               ],
             ),
           ),

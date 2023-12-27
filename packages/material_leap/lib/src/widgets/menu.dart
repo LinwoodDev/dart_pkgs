6287dcf38d586960a109b24b01e893cc74e3a178
CodeDoctorDE <codedoctor@linwood.dev>
2023-12-27 09:53:50 +0100

    Add more menu button types

--- packages/material_leap/lib/src/widgets/menu.dart
+++ packages/material_leap/lib/src/widgets/menu.dart
@@ -16,3 +16,51 @@ MenuAnchorChildBuilder defaultMenuButton({
               ? () => controller.isOpen ? controller.close() : controller.open()
               : null,
         );
+
+MenuAnchorChildBuilder defaultFilledMenuButton({
+  Widget? icon,
+  bool enabled = true,
+  bool? isSelected,
+  String? tooltip,
+}) =>
+    (context, controller, child) => IconButton.filled(
+          icon:
+              icon ?? const PhosphorIcon(PhosphorIconsLight.dotsThreeVertical),
+          tooltip: tooltip,
+          isSelected: isSelected,
+          onPressed: enabled
+              ? () => controller.isOpen ? controller.close() : controller.open()
+              : null,
+        );
+
+MenuAnchorChildBuilder defaultFilledTonalMenuButton({
+  Widget? icon,
+  bool enabled = true,
+  bool? isSelected,
+  String? tooltip,
+}) =>
+    (context, controller, child) => IconButton.filledTonal(
+          icon:
+              icon ?? const PhosphorIcon(PhosphorIconsLight.dotsThreeVertical),
+          tooltip: tooltip,
+          isSelected: isSelected,
+          onPressed: enabled
+              ? () => controller.isOpen ? controller.close() : controller.open()
+              : null,
+        );
+
+MenuAnchorChildBuilder defaultOutlinedMenuButton({
+  Widget? icon,
+  bool enabled = true,
+  bool? isSelected,
+  String? tooltip,
+}) =>
+    (context, controller, child) => IconButton.outlined(
+          icon:
+              icon ?? const PhosphorIcon(PhosphorIconsLight.dotsThreeVertical),
+          tooltip: tooltip,
+          isSelected: isSelected,
+          onPressed: enabled
+              ? () => controller.isOpen ? controller.close() : controller.open()
+              : null,
+        );

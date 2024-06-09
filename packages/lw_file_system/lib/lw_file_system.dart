22062e9093e728e2b228a40598e346eab4a6355c
CodeDoctorDE <codedoctor@linwood.dev>
2024-06-09 20:24:54 +0200

    Add typed to export

--- packages/lw_file_system/lib/lw_file_system.dart
+++ packages/lw_file_system/lib/lw_file_system.dart
@@ -1,9 +1,10 @@
-/// Support for doing something awesome.
+/// Cross platform file system for flutter
 ///
-/// More dartdocs go here.
+/// Use this package to store files in a cross platform way locally or remotely.
 library lw_file_system;
 
 export 'src/api/file_system_base.dart';
+export 'src/api/typed.dart';
 export 'src/models/config.dart';
 export 'src/models/entity.dart';
 export 'src/models/location.dart';

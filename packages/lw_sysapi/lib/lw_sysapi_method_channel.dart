0e04c453c2b6d4aa499f79e54bb1c0b7a9b2c42c
CodeDoctorDE <codedoctor@linwood.dev>
2024-06-23 17:24:08 +0200

    Fix ci

--- packages/lw_sysapi/lib/lw_sysapi_method_channel.dart
+++ packages/lw_sysapi/lib/lw_sysapi_method_channel.dart
@@ -11,7 +11,8 @@ class MethodChannelLwSysapi extends LwSysapiPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version =
+        await methodChannel.invokeMethod<String>('getPlatformVersion');
     return version;
   }
 }

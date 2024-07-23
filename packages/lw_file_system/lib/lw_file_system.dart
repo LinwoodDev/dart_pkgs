efd32e7a26e20a71fcece5c716f49af24771ad6d
CodeDoctorDE <codedoctor@linwood.dev>
2024-07-23 18:48:09 +0200

    Export remote classes

--- packages/lw_file_system/lib/lw_file_system.dart
+++ packages/lw_file_system/lib/lw_file_system.dart
@@ -5,6 +5,7 @@ library lw_file_system;
 
 export 'package:lw_file_system_api/lw_file_system_api.dart';
 export 'src/api/file_system_base.dart';
+export 'src/api/file_system_remote.dart';
 export 'src/api/typed.dart';
 export 'src/models/config.dart';
 export 'src/models/password.dart';

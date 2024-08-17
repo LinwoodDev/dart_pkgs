e85505ee59c7594f5c0c23cc0ae415096effd8fc
CodeDoctorDE <codedoctor@linwood.dev>
2024-08-17 18:26:04 +0200

    Allow reconnecting to socket, add onOpen and onClosed

--- packages/networker/networker_socket/lib/client.dart
+++ packages/networker/networker_socket/lib/client.dart
@@ -1,38 +1,59 @@
+import 'dart:async';
 import 'dart:typed_data';
 
 import 'package:networker/networker.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 class NetworkerSocketClient extends NetworkerClient {
-  final WebSocketChannel webSocketChannel;
+  WebSocketChannel? _channel;
+
   @override
   final Uri address;
+  final Iterable<String>? protocols;
 
-  NetworkerSocketClient(Uri address, {Iterable<String>? protocols})
-      : this.fromChannel(
-            address, WebSocketChannel.connect(address, protocols: protocols));
+  WebSocketChannel? get channel => _channel;
 
-  NetworkerSocketClient.fromChannel(this.address, this.webSocketChannel);
+  final StreamController<void> _onOpen = StreamController<void>.broadcast(),
+      _onClosed = StreamController<void>.broadcast();
 
   @override
-  void init() {
-    webSocketChannel.stream.listen((event) {
+  Stream<void> get onClosed => _onClosed.stream;
+
+  @override
+  Stream<void> get onOpen => _onOpen.stream;
+
+  NetworkerSocketClient(this.address, {this.protocols});
+
+  @override
+  Future<void> init() async {
+    if (isOpen) {
+      return;
+    }
+    final channel =
+        _channel = WebSocketChannel.connect(address, protocols: protocols);
+    channel.stream.listen((event) {
       onMessage(event);
-    });
+    }, onDone: () {
+      _onClosed.add(null);
+    }, onError: (error) {
+      _onClosed.addError(error);
+    }, cancelOnError: true);
+    await channel.ready;
+    _onOpen.add(null);
   }
 
   @override
   void close() {
-    webSocketChannel.sink.close();
+    channel?.sink.close();
   }
 
   @override
-  bool get isClosed => webSocketChannel.closeReason == null;
+  bool get isClosed => channel?.closeReason == null;
 
   @override
   Future<void> sendMessage(Uint8List data, [Channel channel = kAnyChannel]) {
     super.sendMessage(data);
-    webSocketChannel.sink.add(data);
-    return webSocketChannel.sink.done;
+    _channel?.sink.add(data);
+    return _channel?.sink.done ?? Future.value();
   }
 }
--- packages/networker/networker_socket/lib/server.dart
+++ packages/networker/networker_socket/lib/server.dart
@@ -25,30 +25,48 @@ class NetworkerSocketInfo extends ConnectionInfo {
 }
 
 class NetworkerSocketServer extends NetworkerServer<NetworkerSocketInfo> {
-  final HttpServer server;
-  bool _isClosed = false;
+  HttpServer? _server;
+  final SecurityContext? securityContext;
+  final dynamic serverAddress;
+  final int port;
   bool Function(HttpRequest event)? filterConnections;
-  NetworkerSocketServer(this.server, [this.filterConnections]);
+
+  HttpServer? get server => _server;
+
+  NetworkerSocketServer(
+    this.serverAddress,
+    this.port, {
+    this.filterConnections,
+    this.securityContext,
+  });
+
+  final StreamController<void> _onOpen = StreamController<void>.broadcast(),
+      _onClosed = StreamController<void>.broadcast();
+
+  @override
+  Stream<void> get onClosed => _onClosed.stream;
+
+  @override
+  Stream<void> get onOpen => _onOpen.stream;
 
   @override
   void close() {
-    server.close();
-    _isClosed = true;
+    _server?.close();
+    _server = null;
   }
 
   @override
-  bool get isClosed => _isClosed;
+  bool get isClosed => _server != null;
 
   @override
   Uri get address => Uri(
         scheme: 'ws',
-        host: server.address.host,
-        port: server.port,
+        host: _server?.address.host,
+        port: _server?.port,
       );
 
-  @override
-  Future<void> init() async {
-    await for (var request in server.where(filterConnections ?? (e) => true)) {
+  Future<void> _run() async {
+    _server?.where(filterConnections ?? (e) => true).listen((request) async {
       try {
         final socket = await WebSocketTransformer.upgrade(request);
         final id = addClientConnection(
@@ -61,7 +79,29 @@ class NetworkerSocketServer extends NetworkerServer<NetworkerSocketInfo> {
           removeConnection(id);
         });
       } catch (_) {}
+    }, onDone: () {
+      _onClosed.add(null);
+    }, onError: (error) {
+      _onClosed.addError(error);
+    }, cancelOnError: true);
+  }
+
+  @override
+  Future<void> init() async {
+    if (isOpen) {
+      return;
     }
-    _isClosed = true;
+    _server = securityContext == null
+        ? await HttpServer.bind(
+            serverAddress,
+            port,
+          )
+        : await HttpServer.bindSecure(
+            serverAddress,
+            port,
+            securityContext!,
+          );
+    _onOpen.add(null);
+    _run();
   }
 }

78f9b1736d7bf2a58185833273021d1d24deacf4
CodeDoctorDE <codedoctor@linwood.dev>
2024-08-17 22:12:15 +0200

    Add transform to echo pipe

--- packages/networker/networker/lib/src/plugin/echo.dart
+++ packages/networker/networker/lib/src/plugin/echo.dart
@@ -2,12 +2,21 @@ import 'package:networker/networker.dart';
 
 class EchoPipe<T> extends SimpleNetworkerPipe<T> {
   final Channel? toChannel;
+  final T? Function(T data)? transform;
 
-  EchoPipe({this.toChannel});
+  EchoPipe({
+    this.toChannel,
+    this.transform,
+  });
 
   @override
   void onMessage(T data, [Channel channel = kAnyChannel]) {
     super.onMessage(data, channel);
-    super.sendMessage(data, toChannel ?? channel);
+    if (transform != null) {
+      final transformed = transform?.call(data);
+      if (transformed == null) return;
+      data = transformed;
+    }
+    sendMessage(data, toChannel ?? channel);
   }
 }

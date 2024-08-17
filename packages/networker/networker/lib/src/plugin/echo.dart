b5412c6aa819b1921cac5fd3dfff1d998586dd41
CodeDoctorDE <codedoctor@linwood.dev>
2024-08-17 22:26:36 +0200

    Remove transformer in echo, added advancednetworkerpipetransformer instead

--- packages/networker/networker/lib/src/plugin/echo.dart
+++ packages/networker/networker/lib/src/plugin/echo.dart
@@ -2,21 +2,12 @@ import 'package:networker/networker.dart';
 
 class EchoPipe<T> extends SimpleNetworkerPipe<T> {
   final Channel? toChannel;
-  final T? Function(T data)? transform;
 
-  EchoPipe({
-    this.toChannel,
-    this.transform,
-  });
+  EchoPipe({this.toChannel});
 
   @override
   void onMessage(T data, [Channel channel = kAnyChannel]) {
     super.onMessage(data, channel);
-    if (transform != null) {
-      final transformed = transform?.call(data);
-      if (transformed == null) return;
-      data = transformed;
-    }
-    sendMessage(data, toChannel ?? channel);
+    super.sendMessage(data, toChannel ?? channel);
   }
 }
--- packages/networker/networker/lib/src/plugin/simple.dart
+++ packages/networker/networker/lib/src/plugin/simple.dart
@@ -13,6 +13,29 @@ final class NetworkerPipeTransformer<I, O> extends NetworkerPipe<I, O> {
   I encode(O data) => _encode(data);
 }
 
+final class AdvancedNetworkerPipeTransformer<I, O> extends NetworkerPipe<I, O> {
+  final (O, Channel)? Function(I, Channel) _decode;
+  final (I, Channel)? Function(O, Channel) _encode;
+
+  AdvancedNetworkerPipeTransformer(this._decode, this._encode);
+
+  @override
+  (O, Channel)? decodeChannel(I data, Channel channel) =>
+      _decode(data, channel);
+
+  // Not in use, but required to implement the interface
+  @override
+  O decode(I data) => _decode(data, kAnyChannel)!.$1;
+
+  @override
+  (I, Channel)? encodeChannel(O data, Channel channel) =>
+      _encode(data, channel);
+
+  // Not in use, but required to implement the interface
+  @override
+  I encode(O data) => _encode(data, kAnyChannel)!.$1;
+}
+
 final class ReversedNetworkerPipe<I, O> extends NetworkerPipe<I, O> {
   final NetworkerPipe<O, I> pipe;
 

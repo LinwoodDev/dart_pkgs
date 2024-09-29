1771b732aaad508053729cd2a14e10a4e426e397
CodeDoctorDE <codedoctor@linwood.dev>
2024-09-29 12:00:36 +0200

    Add width to save to clipboard text

--- packages/material_leap/lib/src/api/save.dart
+++ packages/material_leap/lib/src/api/save.dart
@@ -2,11 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:material_leap/l10n/leap_localizations.dart';
 
-void saveToClipboard(BuildContext context, String text,
-    {Widget? leading, SnackBarBehavior? behavior}) {
+void saveToClipboard(
+  BuildContext context,
+  String text, {
+  Widget? leading,
+  SnackBarBehavior behavior = SnackBarBehavior.floating,
+  double? width = 300,
+}) {
   Clipboard.setData(ClipboardData(text: text));
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-    behavior: behavior ?? SnackBarBehavior.floating,
+    behavior: behavior,
+    width: width,
     content: Row(mainAxisSize: MainAxisSize.min, children: [
       if (leading != null) ...[
         leading,

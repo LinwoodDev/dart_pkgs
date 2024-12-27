1643305aaeaa40127d74304bad2b088cc1115460
CodeDoctorDE <codedoctor@linwood.dev>
2024-12-27 21:53:15 +0100

    Fix unused import

--- packages/material_leap/lib/src/widgets/window.dart
+++ packages/material_leap/lib/src/widgets/window.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:material_leap/l10n/leap_localizations.dart';
 import 'package:material_leap/material_leap.dart';
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:window_manager/window_manager.dart';

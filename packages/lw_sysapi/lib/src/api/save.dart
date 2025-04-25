8b6b96598aa86036729095677f151ad55c7a923e
CodeDoctorDE <codedoctor@linwood.dev>
2025-04-25 13:12:53 +0200

    Fix save imports

--- packages/lw_sysapi/lib/src/api/save.dart
+++ packages/lw_sysapi/lib/src/api/save.dart
@@ -3,8 +3,8 @@ import 'dart:io';
 import 'package:lw_sysapi/src/api/src/share.dart';
 
 import 'src/save_stub.dart'
-    if (dart.library.io) 'save_io.dart'
-    if (dart.library.js_interop) 'save_html.dart' as save;
+    if (dart.library.io) 'src/save_io.dart'
+    if (dart.library.js_interop) 'src/save_html.dart' as save;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 

426b6af52aa7028264d0d725a89d7cd8102e5dda
CodeDoctorDE <codedoctor@linwood.dev>
2025-05-04 18:00:44 +0200

    Fix import statement for web_socket_stub.dart to correctly alias ws

--- packages/networker/networker_socket/lib/src/web_socket.dart
+++ packages/networker/networker_socket/lib/src/web_socket.dart
@@ -1,8 +1,8 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-import 'web_socket_stub.dart' as ws
+import 'web_socket_stub.dart'
     if (dart.library.js_interop) 'web_socket_html.dart'
-    if (dart.library.io) 'web_socket_io.dart';
+    if (dart.library.io) 'web_socket_io.dart' as ws;
 
 WebSocketChannel constructWebSocketChannel(Uri uri,
     {Iterable<String>? protocols, Duration? pingInterval}) {

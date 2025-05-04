e9eacf474174f89b6d9911e9437d34abab54655e
CodeDoctorDE <codedoctor@linwood.dev>
2025-05-04 17:57:37 +0200

    Fix import condition for web socket html

--- packages/networker/networker_socket/lib/src/web_socket.dart
+++ packages/networker/networker_socket/lib/src/web_socket.dart
@@ -1,7 +1,7 @@
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'web_socket_stub.dart' as ws
-    if (dart.library.html) 'web_socket_html.dart'
+    if (dart.library.js_interop) 'web_socket_html.dart'
     if (dart.library.io) 'web_socket_io.dart';
 
 WebSocketChannel constructWebSocketChannel(Uri uri,
